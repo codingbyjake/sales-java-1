a42643037660841d1cf1a18bad1c75d786923cbd
Jacob Witte <wittejg@gmail.com>
2023-03-30 16:26:34 -0400

    recalculatetotal in progress

--- src/main/java/com/maxtrain/PRS/RequestLine/RequestLineController.java
+++ src/main/java/com/maxtrain/PRS/RequestLine/RequestLineController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import com.maxtrain.PRS.Request.Request;
+import com.maxtrain.PRS.Request.RequestRepository;
 
 @CrossOrigin
 @RestController
@@ -14,6 +16,9 @@ public class RequestLineController {
 	
 	@Autowired
 	private RequestLineRepository reqlineRepo;
+	
+	@Autowired
+	private RequestRepository reqRepo;
 	
 	@GetMapping
 	public ResponseEntity<Iterable<RequestLine>> getAllRequestLines(){
@@ -36,12 +41,14 @@ public class RequestLineController {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
 		RequestLine updatedRequestLine = reqlineRepo.save(requestline);
+		RecalculateRequestTotal(id);													//  >>>>>>>>>>>>>>>  Inserted Call to RecalculateRequestTotal
 		return new ResponseEntity<RequestLine>(updatedRequestLine, HttpStatus.OK);
 	}
 	
 	@PostMapping
 	public ResponseEntity<RequestLine> postRequestLine(@RequestBody RequestLine requestline){
 		RequestLine newRequestLine = reqlineRepo.save(requestline);
+		RecalculateRequestTotal(newRequestLine.getRequest().getId());						//  >>>>>>>>>>>>>>>  Inserted Call to RecalculateRequestTotal
 		return new ResponseEntity<RequestLine>(newRequestLine, HttpStatus.OK);
 	}
 	
@@ -51,9 +58,37 @@ public class RequestLineController {
 		if(requestline.isEmpty()) {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
+		RequestLine goneRequestLine = requestline.get();
 		reqlineRepo.delete(requestline.get());
+		RecalculateRequestTotal(goneRequestLine.getRequest().getId());						//  >>>>>>>>>>>>>>>  Inserted Call to RecalculateRequestTotal
 		return new ResponseEntity<RequestLine>(requestline.get(), HttpStatus.GONE);
 	}
+	
+	// ************* Private Additional Recalculate Request Total Method
+	private boolean RecalculateRequestTotal(int id) {
+		Optional<Request> request = reqRepo.findById(id);
+		if(request.isEmpty()) {
+			return false;
+		}
+		Request foundRequest = request.get();
+		Iterable<RequestLine> associatedRequestLines = reqlineRepo.findByRequestId(id);
+		double newTotal = 0;
+		for(RequestLine rl : associatedRequestLines) {
+			newTotal += rl.getQuantity()*rl.getProduct().getPrice();
+		}
+		foundRequest.setTotal(newTotal);
+		reqRepo.save(foundRequest);
+		return true;
+	}
+	
+	// ************* Private Additional Recalculate Request Total Method blind attempt
+//	private Request RecalculateRequestTotal(int id) {
+//		Optional<RequestLine> reqline = reqlineRepo.findById(id);
+//		Optional<Request> req = reqRepo.findById(reqline.get().getRequest().getId());
+//		req.get().setTotal(req.get().getTotal() + reqline.get().getQuantity()*reqline.get().getProduct().getPrice());
+//		Request updatedRequest = reqRepo.save(req.get());
+//		return updatedRequest;
+//	}
 		
 
 }
--- src/main/java/com/maxtrain/PRS/RequestLine/RequestLineRepository.java
+++ src/main/java/com/maxtrain/PRS/RequestLine/RequestLineRepository.java
@@ -1,7 +1,8 @@
 package com.maxtrain.PRS.RequestLine;
 
+import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 
 public interface RequestLineRepository extends CrudRepository<RequestLine, Integer> {
-
+	Iterable<RequestLine> findByRequestId(int requestId);
 }
